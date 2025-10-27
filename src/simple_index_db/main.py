e0a3ed786ab0bcb281b070846800c814d8be18ab
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-10-27 19:02:55 +0100

    Treat updates and additions separately

--- src/simple_index_db/main.py
+++ src/simple_index_db/main.py
@@ -6,9 +6,13 @@ from threading import Thread
 import typer
 from requests.exceptions import HTTPError
 from sqlalchemy import select
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import defaultload
 
-from .db import Project, init_db
+from .db import (
+    File,
+    Project,
+    init_db,
+)
 from .pypi_client import PyPIClient
 
 app = typer.Typer()
@@ -77,74 +81,97 @@ def find_projects_to_update(Session):
              - projects_to_add: Queue of project names to add.
     """
     repo_last_serial, projects = get_project_list()
+    new_projects = {
+        normalize(project_info["name"]): project_info["_last-serial"]
+        for project_info in projects
+    }
     projects_to_update = Queue()
     num_projects = len(projects)
     num_projects_to_update = 0
     with Session() as session:
-        for project_info in projects: #[:10000]:
-            new_project_last_serial = project_info["_last-serial"]
-            project_name = normalize(project_info["name"])
+        for name, old_last_serial in session.execute(
+            select(Project.name, Project.last_serial)
+        ):
             try:
-                project = session.execute(
-                    select(Project).filter_by(name=project_name)
-                ).scalar_one()
-                old_project_last_serial = project.last_serial
-                if project.last_serial >= new_project_last_serial:
-                    continue
-            except NoResultFound:
-                old_project_last_serial = "unknown"
-                pass
-            # print(f"Queuing project {project_name} for update w/ serial {new_project_last_serial} ({old_project_last_serial})")
-            projects_to_update.put(project_name)
-            num_projects_to_update += 1
-    return num_projects, num_projects_to_update, projects_to_update
-
-
-@app.command()
-def main():
-    Session = init_db()
-    num_projects, numprojects_to_update, projects_to_update = find_projects_to_update(
-        Session
+                new_last_serial = new_projects.pop(name)
+            except KeyError:
+                continue
+            if old_last_serial < new_last_serial:
+                projects_to_update.put(name)
+                num_projects_to_update += 1
+    projects_to_add = Queue()
+    num_projects_to_add = 0
+    for name in new_projects.keys():
+        projects_to_add.put(name)
+        num_projects_to_add += 1
+    return (
+        num_projects,
+        num_projects_to_update,
+        projects_to_update,
+        num_projects_to_add,
+        projects_to_add,
     )
-    print(f"Total projects on PyPI: {num_projects}")
-    print(f"Projects to update: {numprojects_to_update}")
+
+
+def process_updates(Session, project_queue, num_projects, update=False):
     project_info_queue = Queue()
     for _ in range(4):
         Thread(
             target=get_project_info,
-            args=(projects_to_update, project_info_queue),
+            args=(project_queue, project_info_queue),
         ).start()
     start = time.time()
+    num_updated_projects = 0
     with Session() as session:
-        num_updated_projects = 0
         while True:
             try:
                 project_last_serial, project_info = project_info_queue.get(timeout=1)
-                project = session.execute(
-                    select(Project).filter_by(name=project_info["name"])
-                ).scalar_one_or_none()
-                if project is not None:
-                #     project.update_from_info(session, project_last_serial, project_info)
-                # else:
-                    session.delete(project)
-                    session.commit()
-                project = Project.from_info(
-                    session, project_last_serial, project_info
-                )
-                session.add(project)
+                if update:
+                    project = session.execute(
+                        select(Project)
+                        .options(defaultload(Project.files).load_only(File.filename))
+                        .filter_by(name=project_info["name"])
+                    ).scalar_one_or_none()
+                else:
+                    project = None
+                if project is None:
+                    project = Project.from_info(
+                        session, project_last_serial, project_info
+                    )
+                    session.add(project)
+                else:
+                    project.update_from_info(session, project_last_serial, project_info)
                 project_info_queue.task_done()
                 num_updated_projects += 1
             except Empty:
-                if projects_to_update.empty():
+                if project_queue.empty():
                     break
             if num_updated_projects % 100 == 0:
                 session.commit()
                 end = time.time()
                 elapsed = end - start
-                percent = num_updated_projects / numprojects_to_update * 100.
+                percent = num_updated_projects / num_projects * 100.0
                 print(
-                    f"Updated {num_updated_projects}/{numprojects_to_update} projects ({percent}%) "
-                    f"in {elapsed:.2f} seconds (ETT: {elapsed / percent * 100.:.2f} seconds)"
+                    f"Updated {num_updated_projects}/{num_projects} projects ({percent:.2f}%) "
+                    f"in {elapsed:.2f} seconds (ETT: {elapsed / percent * 100.0:.2f} seconds)"
                 )
         session.commit()
-        print(f"Committed {num_updated_projects} updated projects")
+    print(f"Committed {num_updated_projects} updated projects")
+
+
+@app.command()
+def main():
+    Session = init_db()
+    (
+        num_projects,
+        num_projects_to_update,
+        projects_to_update,
+        num_projects_to_add,
+        projects_to_add,
+    ) = find_projects_to_update(Session)
+    print(f"Total projects on PyPI: {num_projects}")
+    print(f"Projects to update: {num_projects_to_update}")
+    print(f"Projects to add: {num_projects_to_add}")
+
+    process_updates(Session, projects_to_update, num_projects_to_update, update=True)
+    process_updates(Session, projects_to_add, num_projects_to_add, update=False)
